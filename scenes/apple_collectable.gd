70f64935e50f4942f2adb400d09be2c775430601
evgeniy.pomortsev <evgeniy.pomortsev@acronis.com>
2025-09-03 15:30:17 +0200

    Add apple tree with falling apple, ability to trigger the fall and custom fall animation via AnimationPlayer.

---
+++ scenes/apple_collectable.gd
@@ -0,0 +1,12 @@
+extends StaticBody2D
+
+func _ready():
+	fall_from_tree()
+	
+func fall_from_tree():
+	$AnimationPlayer.play("falling_from_tree")
+	await get_tree().create_timer(1.5).timeout  # time for apple to lay on the ground before it fades
+	$AnimationPlayer.play("fade")
+	print("+1 apple")
+	await get_tree().create_timer(0.3).timeout  # time for apple to fade
+	queue_free()
---
+++ script/apple_tree.gd
@@ -0,0 +1,44 @@
+extends Node2D
+
+var state = "no_apples"
+var player_in_area = false
+
+var apple = preload("res://scenes/apple_collectable.tscn")
+
+func _ready():
+	if state == "no_apples":
+		$growth_timer.start()
+
+func _process(delta):
+	if state == "no_apples":
+		$AnimatedSprite2D.play("no_apples")
+	if state == "apples":
+		$AnimatedSprite2D.play("apples")
+		if player_in_area:
+			if Input.is_action_just_pressed("use"):
+				state = "no_apples"
+				drop_apple()
+
+
+func _on_pick_area_body_entered(body: Node2D) -> void:
+	if body.has_method("PlayerCharacter"):
+		player_in_area = true
+
+
+func _on_pick_area_body_exited(body: Node2D) -> void:
+	if body.has_method("PlayerCharacter"):
+		player_in_area = false
+
+
+func _on_growth_timer_timeout() -> void:
+	if state == "no_apples":
+		state = "apples"
+
+
+func drop_apple():
+	var apple_instance = apple.instantiate()
+	apple_instance.global_position = $Marker2D.global_position
+	get_parent().add_child(apple_instance)
+	await get_tree().create_timer(3).timeout  # time for apple to stay on screen
+	$growth_timer.start()
+	
